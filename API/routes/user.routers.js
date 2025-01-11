fd1977cc7e1d8b5cdb6fe8376ad757e61db72873
alhamkbaha <alhamkbaha7@gmail.com>
2025-01-11 04:56:25 -0800

    asd

--- API/routes/user.routers.js
+++ API/routes/user.routers.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const userRouter = express.Router();
-const { createUser, FindUser, DeletUser, updateuser } = require("../controllers/user.controllers");
+const { createUser, FindUser, DeletUser, updateuser, Login } = require("../controllers/user.controllers");
 
 userRouter.post("/createUser", createUser);
+userRouter.patch("/ login",  Login)
 
 userRouter.post("/FindUser", FindUser)
 userRouter.delete("/DeletUser", DeletUser)
